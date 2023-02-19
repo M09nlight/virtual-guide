938fb8fdc8d37a86d1e007a151ad1140351f7d2f
M09nlight <ivan.portyanko@yandex.ru>
2023-02-19 22:57:35 +0300

    feat: add info for minsk region

---
+++ eat/mogilev/Mogilev/Mogilev.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Belarus</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <link href="../../../css/style.css" rel="stylesheet">
+
+    </link>
+
+<body>
+    <?php include('../../../fragments/header.php'); ?>
+    <div class="container about">
+        <section class="container">
+            <div class="box">
+                <h3>Mogilev</h3>
+
+                <div class="box-container">
+
+                    <div class="box-eat">
+                        <div class="image">
+                            <img src="../../../images/eat/mogilev/7_.jpg" alt="">
+                        </div>
+                        <div class="content">
+                            <div class="head">Cafe "Buffet"</div>
+                            <p class="address">street Komsomolskaya, 5, Ordzhonikidze square</p>
+                            <p class="text-left-30">For breakfast or a snack, choose hearty sandwiches, cheesecakes or
+                                pancakes, but for lunch or dinner, in addition to burgers and pizza, you will be offered
+                                pasta, dumplings, dumplings, capilletti here ... In general, in order to try everything,
+                                you need to visit here more than once.</p>
+                        </div>
+                    </div>
+                    <div class="box-eat">
+                        <div class="image">
+                            <img src="../../../images/eat/mogilev/8_.jpg" alt="">
+                        </div>
+                        <div class="content">
+                            <div class="head">Burger Pizza</div>
+                            <p class="text-left-30">Burger Pizza cafes are scattered throughout the city. In the unified
+                                menu you will find pizza, burgers, various salads and hot dishes. Burgers, by the way,
+                                the guys are constantly "pumping" and they are getting better and better. Perfect for a
+                                family lunch or dinner. You can eat here for € 5-10.</p>
+                        </div>
+                    </div>
+                    <div class="box-eat">
+                        <div class="image">
+                            <img src="../../../images/eat/mogilev/9_.jpg" alt="">
+                        </div>
+                        <div class="content">
+                            <div class="head">The Van Gogh cafe</div>
+                            <p class="address">36, Leninskaya St.</p>
+                            <p class="text-left-30">The cafe has a leisurely rhythm and interesting-tasting dishes with
+                                a pleasant presentation. On weekdays, take advantage of the lunch menu or the discount
+                                that applies to the main menu from 15.00 to 18.00. Van Gogh paintings are inscribed in
+                                the interior, as if from a dollhouse, and old films are played on TV. There are no ears
+                                on the menu, we checked.</p>
+                        </div>
+                    </div>
+                    <div class="box-eat">
+                        <div class="image">
+                            <img src="../../../images/eat/mogilev/10_.jpg" alt="">
+                        </div>
+                        <div class="content">
+                            <div class="head">Restaurant "101"</div>
+                            <p class="address">4 Pozharny Pereulok</p>
+                            <p class="text-left-30">It is located on the site of an earlier operating fire station and
+                                is filled with firefighter paraphernalia. The staff is well-trained and in uniform, and
+                                a fire engine is built into the bar counter, with which you can take pictures. Beer will
+                                be poured from a makeshift fire extinguisher.</p>
+                        </div>
+                    </div>
+                    <div class="box-eat">
+                        <div class="image">
+                            <img src="../../../images/eat/mogilev/11_.jpg" alt="">
+                        </div>
+                        <div class="content">
+                            <div class="head">Bellagio restaurant</div>
+                            <p class="address">Leninskaya st., 21</p>
+                            <p class="text-left-30">Visiting artists like to visit here and respectable residents of
+                                Mogilev like to arrange dates. But the place is quite democratic. It's delicious and
+                                delicious here.</p>
+                        </div>
+                    </div>
+                    <div class="box-eat">
+                        <div class="image">
+                            <img src="../../../images/eat/mogilev/12_.jpg" alt="">
+                        </div>
+                        <div class="content">
+                            <div class="head">Parmesan</div>
+                            <p class="address">Pervomaiskaya St., 34/1</p>
+                            <p class="text-left-30">Here you will find not only pizza, but it will be the tastiest of
+                                all. The place is not cheap, but atmospheric. Until recently, Parmesan was considered
+                                the best in terms of service in the city (no, it didn’t get worse, just many
+                                alternatives appeared). A favorite meeting place for expats in Mogilev. A foreign
+                                language will not be surprised here and they will offer a menu translated into English.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="box-eat">
+                        <div class="image">
+                            <img src="../../../images/eat/mogilev/13_.jpg" alt="">
+                        </div>
+                        <div class="content">
+                            <div class="head">Bakehouse cafe</div>
+                            <p class="address">Pervomaiskaya St., 34/1</p>
+                            <p class="text-left-30">There are institutions in Mogilev, which some locals jokingly call
+                                "teleports". It's all about the atmosphere that is created there. You will be simply
+                                pleased to drink a cup of delicious coffee and choose something for dessert at the
+                                Bakehouse cafe - there are many places and
+                                you can watch the bustle of the main street through a large window.</p>
+                        </div>
+                    </div>
+                    <div class="box-eat">
+                        <div class="image">
+                            <img src="../../../images/eat/mogilev/14_.jpg" alt="">
+                        </div>
+                        <div class="content">
+                            <div class="head">cafe "Respublika"</div>
+                            <p class="address">Leninskaya st., 30</p>
+                            <p class="text-left-30">In the cafe "Respublika"
+                                there are fewer seats, but this only makes it more comfortable. Sometimes there are
+                                exhibitions and musicians perform here.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
+    <script src="../../../js/google-translate.js"></script>
+    <script src="//translate.google.com/translate_a/element.js?cb=TranslateInit"></script>
+    <script src="../../../js/script.js"></script>
+</body>
+
+</html>
---
+++ hotels/mogilev/Mogilev/Mogilev.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Belarus</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <link href="../../../css/style.css" rel="stylesheet">
+
+    </link>
+
+<body>
+    <?php include('../../../fragments/header.php'); ?>
+    <div class="container about">
+        <section class="container">
+            <div class="box">
+                <h3>Mogilev</h3>
+
+                <div class="box-container">
+
+                    <div class="box-eat">
+                        <div class="image">
+                            <img src="../../../images/hotels/mogilev/24_.jpg" alt="">
+                        </div>
+                        <div class="content">
+                            <div class="head">Hotel Mogilev</div>
+                            <p class="address">Prospect Mira</p>
+                            <p class="text-left-30">Large selection of rooms, the cheapest and simplest costs € 12 per
+                                night. Decent breakfasts and an all-day cafe. Paid parking for a car or a convenient
+                                point of departure to Minsk at your service.</p>
+                        </div>
+                    </div>
+                    <div class="box-eat">
+                        <div class="image">
+                            <img src="../../../images/hotels/mogilev/25_.jpg" alt="">
+                        </div>
+                        <div class="content">
+                            <div class="head">Hotel "Gubernskaya"</div>
+                            <p class="address">st. Leninskaya</p>
+                            <p class="text-left-30">(Favorable proximity to the square with fountains, drinking
+                                establishments and direct access to the pedestrian street. Nearby is a futuristic
+                                business center and a stadium.</p>
+                        </div>
+                    </div>
+                    <div class="box-eat">
+                        <div class="image">
+                            <img src="../../../images/hotels/mogilev/26_.jpg" alt="">
+                        </div>
+                        <div class="content">
+                            <div class="head">Lira</div>
+                            <p class="address">45 Leninskaya St.</p>
+                            <p class="text-left-30">Calls itself a boutique hotel, hence the prices. A small hotel right
+                                on the pedestrian main street, there are several nice cafes, shops, a cinema and Star
+                                Square nearby.</p>
+                        </div>
+                    </div>
+                    <div class="box-eat">
+                        <div class="image">
+                            <img src="../../../images/hotels/mogilev/27_.jpg" alt="">
+                        </div>
+                        <div class="content">
+                            <div class="head">Metropol</div>
+                            <p class="address">6 Komsomolskaya St.</p>
+                            <p class="text-left-30">A decent and expensive hotel in the very heart of the city. Near the
+                                Drama Theatre, the Church of St. Stanislaus, a pedestrian street. The hotel has a spa.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="box-eat">
+                        <div class="image">
+                            <img src="../../../images/hotels/mogilev/28_.jpg" alt="">
+                        </div>
+                        <div class="content">
+                            <div class="head">"Tourist"</div>
+                            <p class="address">pr-t. Pushkin, 6</p>
+                            <p class="text-left-30">Near the embankment of the Dnieper, the ice palace and one of the
+                                main concert venues of the city. Therefore, the hotel is quite suitable for spending the
+                                night after the concert, which is usually cheaper in Mogilev than in Minsk and other
+                                regions.</p>
+                        </div>
+                    </div>
+                    <div class="box-eat">
+                        <div class="image">
+                            <img src="../../../images/hotels/mogilev/29_.jpg" alt="">
+                        </div>
+                        <div class="content">
+                            <div class="head">"Kim"</div>
+                            <p class="address">pr-t. Pushkin, 6</p>
+                            <p class="text-left-30">This is a small, neat hotel within walking distance from the city
+                                center.
+                                This option is interesting in that in addition to ordinary rooms, there is the
+                                possibility of accommodation in a hostel, in a dormitory room. The price of a bed is
+                                about €10.</p>
+                        </div>
+                    </div>
+                    <div class="box-eat">
+                        <div class="image">
+                            <img src="../../../images/hotels/mogilev/30_.jpg" alt="">
+                        </div>
+                        <div class="content">
+                            <div class="head">The Khimvolokno Hotel</div>
+                            <p class="address">pr-t. Pushkin, 6</p>
+                            <p class="text-left-30">This is perhaps not the most glamorous and convenient option in
+                                terms of communication, but definitely exotic. Imagine, in a typical urban development
+                                area, one of the Khrushchevs turns out to be quite a hotel. Where the reception is
+                                located in the first entrance, and your number may be, say, in the fourth. But here it
+                                is quite budgetary, many rooms have a kitchen and everything you need for a long stay.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
+    <script src="../../../js/google-translate.js"></script>
+    <script src="//translate.google.com/translate_a/element.js?cb=TranslateInit"></script>
+    <script src="../../../js/script.js"></script>
+</body>
+
+</html>
